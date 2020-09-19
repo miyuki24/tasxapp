a5f2eb632b1ba85bb5b09672719394c83f479a82
miyuki24 <miyuki.0424@yahoo.ne.jp>
2020-09-19 16:43:17 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by 田中美幸 on 2020/09/19.
+//  Copyright © 2020 miyuki.tanaka2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
